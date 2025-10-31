2555229a102b1ed438acf995aaafa10e39caa478
inbillsworld <sebastianoteroproduction@gmail.com>
2025-10-31 17:08:07 -0300

    Fix import statement for restituirAUREO

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { restituirAUREO } from './src/core/invocador'
 import { activarAUREO } from './core/activacionCompleta'
+import { restituirAUREO } from './core/restituirAUREO'
+restituirAUREO()
 activarAUREO()
 
 const AUREO = restituirAUREO()
