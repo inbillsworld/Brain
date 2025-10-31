51c503ef5a5c7f928c86067b0860123933713465
inbillsworld <sebastianoteroproduction@gmail.com>
2025-10-31 17:03:08 -0300

    Import and activate AUREO in main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import { restituirAUREO } from './src/core/invocador'
+import { activarAUREO } from './core/activacionCompleta'
+activarAUREO()
 
 const AUREO = restituirAUREO()
 console.log('AUREO activado:', AUREO)
